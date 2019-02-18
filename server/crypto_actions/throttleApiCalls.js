5e8133b0343166882b2c678c1e74ad6889ccee18
Akyuna Akish <akyunaakish@gmail.com>
2019-02-18 10:49:16 -0700

    throttle api calls

--- server/crypto_actions/throttleApiCalls.js
+++ server/crypto_actions/throttleApiCalls.js
@@ -3,7 +3,7 @@ const fetchCoin = require('./fetchCoin');
 module.exports = () => {
     return new Promise((resolve, reject) => {
         const fetchPromises = [];
-
+        
         const coinsToFetch = [
             { name: 'ethereum', exchange: 'coinCap' },
             { name: 'litecoin', exchange: 'coinCap' },
